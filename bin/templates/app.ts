d01c629692d7b54f5fad0a44923b180ba7ce17f6
hyunwoo <h.hyunwoo@gmail.com>
2019-09-16 21:21:47 +0900

    index.ts lint 수정

--- bin/templates/app.ts
+++
@@ -1,8 +0,0 @@
-import { Vue, Component } from 'vue-property-decorator';
-
-@Component({})
-export default class {{ App }} extends Vue {
-  private mounted(){
-
-  }
-}
--- bin/templates/app.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <h1>{{ app }}</h1>
-</template>
-<script src='./{{ app }}.ts'>
-
-</script>
-<style lang='scss' scoped>
-@import "./{{ app }}.scss";
-</style>
